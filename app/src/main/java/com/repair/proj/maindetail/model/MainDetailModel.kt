28b7c30cf587b5dd7cb271f5097c45e145f20f4c
niexueliang <code_nil@163.com>
2017-10-23 16:39:08 +0800

    maindetail相关界面的更新

---
+++ app/src/main/java/com/repair/proj/maindetail/model/MainDetailModel.kt
@@ -0,0 +1,12 @@
+package com.repair.proj.maindetail.model
+
+import com.repair.proj.maindetail.contract.MainDetailContract
+
+/**
+ * 说明：
+ * Created by code_nil on 2017/10/23.
+ */
+
+class MainDetailModel : MainDetailContract.Model{
+
+}
