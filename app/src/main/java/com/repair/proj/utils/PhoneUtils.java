a50df394d0149f33352c61af1d81f9c205066170
luojianfei <Leo_jf@163.com>
2017-10-23 09:59:37 +0800

    更新

---
+++ app/src/main/java/com/repair/proj/utils/PhoneUtils.java
@@ -0,0 +1,59 @@
+package com.repair.proj.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+
+import com.repair.proj.ContactInfo;
+
+/**
+ * Created by HX·罗 on 2017/10/23.
+ */
+
+public class PhoneUtils {
+    /**
+     * 根据intent 获取电话本信息
+     * @param context
+     * @param intent
+     * @return
+     */
+    public static ContactInfo getPhoneNumber(Context context, Intent intent) {
+        Cursor cursor = null;
+        Cursor phone = null;
+        ContactInfo contactInfo = new ContactInfo();
+        try {
+            String[] projections = {ContactsContract.Contacts._ID, ContactsContract.Contacts.HAS_PHONE_NUMBER};
+            cursor = context.getContentResolver().query(intent.getData(), projections, null, null, null);
+            if ((cursor == null) || (!cursor.moveToFirst())) {
+                return null;
+            }
+            int _id = cursor.getColumnIndexOrThrow(ContactsContract.Contacts._ID);
+            String id = cursor.getString(_id);
+            int has_phone_number = cursor.getColumnIndexOrThrow(ContactsContract.Contacts.HAS_PHONE_NUMBER);
+            int hasPhoneNumber = cursor.getInt(has_phone_number);
+            if (hasPhoneNumber > 0) {
+                phone = context.getContentResolver().query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        null,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "="
+                                + id, null, null);
+                while (phone.moveToNext()) {
+                    int numberIndex = phone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                    int nameIndex = phone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+                    String number = phone.getString(numberIndex);
+                    String name = phone.getString(nameIndex);
+                    contactInfo.setName(name);
+                    contactInfo.setPhoneNo(number);
+                }
+            }
+            return contactInfo;
+        } catch (Exception e) {
+
+        } finally {
+            if (cursor != null) cursor.close();
+            if (phone != null) phone.close();
+        }
+        return null;
+    }
+}
--- app/src/main/java/com/repair/proj/viewl/SureOrderActivity.java
+++ app/src/main/java/com/repair/proj/viewl/SureOrderActivity.java
@@ -1,17 +1,15 @@
 package com.repair.proj.viewl;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.view.View;
 
-import com.repair.proj.ContactInfo;
 import com.repair.proj.R;
 import com.repair.proj.base.BaseActivity;
 import com.repair.proj.databinding.ActivitySureOrderBinding;
 import com.repair.proj.utils.ActivityUtils;
+import com.repair.proj.utils.PhoneUtils;
 
 /**
  * 确定订单
@@ -63,47 +61,10 @@ public class SureOrderActivity extends BaseActivity<ActivitySureOrderBinding> {
         super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
             case 1://获取联系人
-                viewBinding.setContactInfo(getPhoneNumber(data));
+                viewBinding.setContactInfo(PhoneUtils.getPhoneNumber(context,data));
                 break;
         }
     }
 
-    private ContactInfo getPhoneNumber(Intent intent) {
-        Cursor cursor = null;
-        Cursor phone = null;
-        ContactInfo contactInfo = new ContactInfo();
-        try {
-            String[] projections = {ContactsContract.Contacts._ID, ContactsContract.Contacts.HAS_PHONE_NUMBER};
-            cursor = getContentResolver().query(intent.getData(), projections, null, null, null);
-            if ((cursor == null) || (!cursor.moveToFirst())) {
-                return null;
-            }
-            int _id = cursor.getColumnIndexOrThrow(ContactsContract.Contacts._ID);
-            String id = cursor.getString(_id);
-            int has_phone_number = cursor.getColumnIndexOrThrow(ContactsContract.Contacts.HAS_PHONE_NUMBER);
-            int hasPhoneNumber = cursor.getInt(has_phone_number);
-            if (hasPhoneNumber > 0) {
-                phone = getContentResolver().query(
-                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                        null,
-                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "="
-                                + id, null, null);
-                while (phone.moveToNext()) {
-                    int numberIndex = phone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
-                    int nameIndex = phone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
-                    String number = phone.getString(numberIndex);
-                    String name = phone.getString(nameIndex);
-                    contactInfo.setName(name);
-                    contactInfo.setPhoneNo(number);
-                }
-            }
-            return contactInfo;
-        } catch (Exception e) {
 
-        } finally {
-            if (cursor != null) cursor.close();
-            if (phone != null) phone.close();
-        }
-        return null;
-    }
 }
