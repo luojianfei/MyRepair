161a5a647abc0b40fba367dc5073d4c6aa608ad0
niexueliang <code_nil@163.com>
2017-10-25 13:26:27 +0800

    第二个注册界面

---
+++ app/src/main/java/com/repair/proj/login/RegistSecondActivity.kt
@@ -0,0 +1,51 @@
+package com.repair.proj.login
+
+import android.graphics.Typeface
+import android.support.v7.widget.LinearLayoutManager
+import com.repair.proj.R
+import com.repair.proj.login.adapter.LineTimeAdapter
+import com.repair.proj.login.adapter.StepEnum
+import com.repair.proj.maindetail.contract.MainDetailContract
+import com.repair.proj.maindetail.presenter.MainDetailPresenter
+import io.xujiaji.xmvp.view.base.XBaseActivity
+import kotlinx.android.synthetic.main.activity_regist_second.*
+import org.jetbrains.anko.AnkoLogger
+
+/**
+ * Created by Mwh on 2017/10/25.
+ */
+class RegistSecondActivity : XBaseActivity<MainDetailPresenter>(), MainDetailContract.View, AnkoLogger {
+    override fun getContentId(): Int {
+        return R.layout.activity_regist_second
+    }
+
+    override fun beforeSetContentView() {
+        super.beforeSetContentView()
+    }
+
+    override fun onInit() {
+        super.onInit()
+        //标题字体
+        ars_h_title.typeface = Typeface.DEFAULT_BOLD
+        //初始化recylerview
+        val layoutManager = object : LinearLayoutManager(this) {
+            override fun canScrollHorizontally(): Boolean {
+                return false
+            }
+
+            override fun canScrollVertically(): Boolean {
+                return false
+            }
+        }
+        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
+        ars_rv.layoutManager = layoutManager
+        ars_rv.adapter = LineTimeAdapter(this, StepEnum.STEP4)
+    }
+
+    override fun onListener() {
+        super.onListener()
+        ars_h_cancle.setOnClickListener { }
+    }
+
+
+}
---
+++ app/src/main/java/com/repair/proj/login/adapter/StepEnum.kt
@@ -0,0 +1,9 @@
+package com.repair.proj.login.adapter
+
+/**
+ * 针对枚举类型传入的参数名就是他获取值得参数名
+ * Created by Mwh on 2017/10/25.
+ */
+enum class StepEnum(val step: Int) {
+    STEP1(0), STEP2(1), STEP3(2), STEP4(3)
+}
