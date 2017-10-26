0b37507552ea8ee7656138d108fc0a7089430776
niexueliang <code_nil@163.com>
2017-10-26 18:18:48 +0800

    viewpicker

--- app/src/main/java/com/repair/proj/login/presenter/RegistSecondPresenter.kt
+++ app/src/main/java/com/repair/proj/login/presenter/RegistSecondPresenter.kt
@@ -9,16 +9,17 @@ import io.xujiaji.xmvp.presenters.XBasePresenter
 import kotlinx.coroutines.experimental.*
 
 /**
- * Created by HX·罗 on 2017/10/23.
+ * nie
+ * Created by nxl on 2017/10/23.
  */
 
 class RegistSecondPresenter : XBasePresenter<RegistSecondContract.View, RegistSecondModel>(), RegistSecondContract.Presenter {
     var cityCommon = CityCommon()
     override fun showLocationPicker(activity: Activity) = runBlocking {
         //show loading
-        var dialog = AlertDialog.Builder(activity).setTitle("加载").setMessage("等待城市数据加载").create()
+        val dialog = AlertDialog.Builder(activity).setTitle("加载").setMessage("等待城市数据加载").create()
         dialog.show()
-        var job = async(CommonPool) {
+        val job = launch(CommonPool) {
             cityCommon.initJsonData(activity)
         }
         job.join()
